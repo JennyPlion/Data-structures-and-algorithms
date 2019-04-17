85c91a3d876001769da3e1d9d84b6e2d3d18624e
JennyPlion <phuongtracy27@gmail.com>
2019-04-17 10:16:53 +0700

    Create Bai3-ThayPhuong.cpp

---
+++ Bai3-ThayPhuong.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int C(int k,int n)
+{
+	if(k==0||k==n)return 1;
+	if(k==1)return n;
+	return (C(k-1,n-1)+C(k,n-1));
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	int n,k;
+	while(t--)
+	{
+		cin>>n>>k;
+		cout<<C(k,n+k-1)<<endl;
+	}
+	return 0;
+}
